fcfdd191d8253ddb412e0d1bf3de905d1b34ef43
Aydin Battal <battalay@sheridancollege.ca>
2020-02-05 14:27:24 -0500

    The user interaction is implemented at this point. The players are able to roll dices when its their turn and earn points based on the dices they rolled.

---
+++ DiceeApp_AydinBattal/Game.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceeApp_AydinBattal
+{
+    class Game
+    {
+        public int turnScore;
+        public int player1Points;
+        public int player2Points;
+        public bool IsPlayer1;
+        public List<int> Roll()
+        {
+            Random randomNumberGenerator = new Random();
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < 2; i++)
+            {
+                numbers.Add(randomNumberGenerator.Next(1, 7));
+
+
+            }
+
+            CalculateTurn(numbers);
+            return numbers;
+            
+        }
+
+        private void CalculateTurn(List<int> numbers)
+        {
+            if (numbers[0] == numbers[1])
+            {
+                if (numbers[0] == 6 && numbers[1] == 6)
+                {
+                    turnScore = 100;
+                }
+                else
+                {
+                    turnScore = numbers[0] * 10;
+                }
+            }
+            else
+            {
+                turnScore = 0;
+            }
+            
+            
+        }
+
+        public void PlayerTurn()
+        {
+            IsPlayer1 = true;
+            Roll();
+            IsPlayer1 = false;
+            Roll();
+        }
+    }
+}
--- DiceeApp_AydinBattal/MainPage.xaml.cs
+++ DiceeApp_AydinBattal/MainPage.xaml.cs
@@ -22,6 +22,7 @@ namespace DiceeApp_AydinBattal
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private Game _game = new Game();
         public MainPage()
         {
             this.InitializeComponent();
@@ -29,6 +30,30 @@ namespace DiceeApp_AydinBattal
 
         private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
         {
+            List<int> numbers = _game.Roll();
+            Random random = new Random();
+            Dice1.Text = numbers[0].ToString();
+            Dice2.Text = numbers[1].ToString();
+            //this works too but numbers list not needed
+            //Dice1.Text = random.Next(1, 7).ToString();
+            //Dice2.Text = random.Next(1, 7).ToString();
+
+            TurnScore.Text = $"Joe: {_game.turnScore}";
+
+            if (_game.IsPlayer1 == true)
+            {
+                _game.player1Points = (_game.player1Points + _game.turnScore);
+                RollButton.Content = $"Roll (Player 1)"; 
+                Player1Score.Text = $"Player 1 Score: {_game.player1Points}";
+                _game.IsPlayer1 = false;
+            }
+            else
+            {
+                _game.player2Points = (_game.player2Points + _game.turnScore);
+                RollButton.Content = $"Roll (Player 2)";
+                Player2Score.Text = $"Player 2 Score: {_game.player2Points}";
+                _game.IsPlayer1 = true;
+            }
         }
     }
 }
